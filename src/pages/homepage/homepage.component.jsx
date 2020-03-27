d0293728f0b9ce06a4f01da2b969f418226b4a51
anisimovv <kirill.anisimovv@gmail.com>
2020-03-27 16:35:28 +0200

    collection preview and collection items

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './homepage.styles.scss';
-import Directory from '../directory/directory-menu.component';
+import Directory from '../../component/directory/directory.component';
 
 const HomePage = () => (
   <div className='homepage'>
