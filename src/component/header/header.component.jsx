b11840bdc47115e65cda34e2ea640ebce5649180
anisimovv <kirill.anisimovv@gmail.com>
2020-03-28 22:10:33 +0200

    Added redux and implemented user reducer and actions

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -6,6 +6,7 @@ import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { connect } from 'react-redux';
 
 const Header = ({ currentUser }) => {
   return (
@@ -31,4 +32,8 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
