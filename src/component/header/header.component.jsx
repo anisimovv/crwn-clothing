45ad8b2d815aea4397fb4ada5b6a386a9ee8c226
anisimovv <kirill.anisimovv@gmail.com>
2020-04-01 01:03:26 +0300

    trying styled components

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import { auth } from '../../firebase/firebase.utils';
-
-import './header.styles.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { connect } from 'react-redux';
@@ -12,32 +8,38 @@ import CartIcon from '../cart-icon/cart-icon.conponent';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionLink,
+  OptionsContainer
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => {
   return (
-    <div className='header'>
-      <Link className='logo-container' to='/'>
+    <HeaderContainer>
+      <LogoContainer className='logo-container' to='/'>
         <Logo className='logo'/>
-      </Link>
-      <div className="options">
-        <Link className='option' to='/shop'>
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to='/shop'>
           SHOP
-        </Link>
-        <Link className='option' to='/shop'>
+        </OptionLink>
+        <OptionLink to='/shop'>
           CONTACT
-        </Link>
+        </OptionLink>
         {
           currentUser ?
-            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+            <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
             :
-            <Link className='option' to='/signin'>SIGN IN</Link>
+            <OptionLink to='/signin'>SIGN IN</OptionLink>
         }
         <CartIcon />
-      </div>
+      </OptionsContainer>
       {
         hidden ? null : <CartDropdown />
       }
-    </div>
+    </HeaderContainer>
   );
 };
 
